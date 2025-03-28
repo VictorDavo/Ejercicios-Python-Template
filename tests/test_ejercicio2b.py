778911caa45d3b0f1012f832de0ddd58b912ccb8
Víctor <vicdavant@alu.edu.gva.es>
2025-03-28 12:24:22 +0100

    Solucionados errores tests 2b y 5

--- tests/test_ejercicio2b.py
+++ tests/test_ejercicio2b.py
@@ -7,6 +7,6 @@ import os, sys
 import ejercicio2b
 
 def test_esMayorEdad():
-    assert ejercicio2b.esMayorEdad(5) == False
-    assert ejercicio2b.esMayorEdad(18) == True
-    assert ejercicio2b.esMayorEdad(21) == True
+    assert ejercicio2b.esMayorEdad(5, "Jorge") == False
+    assert ejercicio2b.esMayorEdad(18, "Pablo") == True
+    assert ejercicio2b.esMayorEdad(21, "Carla") == True
--- tests/test_ejercicio5.py
+++ tests/test_ejercicio5.py
@@ -18,8 +18,8 @@ def test_esImpar():
 def test_generarImpares():
     # Este puede no ser un buen caso de prueba por la forma en que esta construído.
     inicio=random.randint(-10000,10000)
-    n=random.randint(0,100000);
-    impares=t.generarImpares(n,inicio);
+    n=random.randint(0,100000)
+    impares=t.generarImpares(n,inicio)
     assert len(impares) == n
     if inicio % 2 ==0 :
         assert impares[0] == inicio+1
@@ -27,19 +27,18 @@ def test_generarImpares():
     esperado=[1, 3, 5, 7, 9]
     assert t.generarImpares(5,1) == esperado
     esperado=[3, 5, 7, 9, 11]
-    assert t.generarImpares(5,2) == esperado
+    assert t.generarImpares(5,3) == esperado
 
 def test_generarPares():
     # Este puede no ser un buen caso de prueba por la forma en que esta construído.
     inicio=random.randint(-10000,10000)
-    n=random.randint(0,100000);
-    pares=t.generarPares(n,inicio);
+    n=random.randint(0,100000)
+    pares=t.generarPares(n,inicio)
     assert len(pares) == n
     if inicio % 2 ==1 :
         assert pares[0] == inicio+1
     # Comprobamos la generación de pares
     esperado=[2, 4, 6, 8, 10]
-    assert t.generarPares(5,1) == esperado
+    assert t.generarPares(5,2) == esperado
     esperado=[4, 6, 8, 10, 12]
     assert t.generarPares(5,4) == esperado
-
